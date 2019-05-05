ab49d91d27f05830df737390eb27de9938502d79
JordonPhillips <phjordon@amazon.com>
2019-05-05 10:00:19 -0400

    Remove extraneous code

--- manualsample.py
+++
@@ -1,8 +0,0 @@
-import botocore.session
-
-sess = botocore.session.get_session()
-client = sess.create_client('sts')
-assert isinstance(client, botocore.client.STS)
-
-response = client.get_caller_identity()
-foo = response['bar']
